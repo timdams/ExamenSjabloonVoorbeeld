4a52058903d8a0053d184de6eaf8f4f2a585adc6
Tim Dams <dams.tim@telenet.be>
2023-11-22 10:18:34 +0100

    Add project files.

---
+++ ExamenSjabloon/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ExamenSjabloonVoorbeeld
+{
+
+
+    //OPGELET DIT IS EEN VOORBEELDSJABLOON
+    //GEBRUIK DIT SJABLOON NIET TIJDENS HET EXAMEN!!!!!!!!!!!!!!!!!!!!!!!
+
+    //VUL VOLGENDE INFORMATIE HIER IN
+    //VERVANG ENKEL DE TEKST NA IEDER DUBBELPUNT!!!!!!!!
+
+    //S-nummer              :  S00478
+    //Voornaam              :  Jos   
+    //Achternaam            :  Dams         
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Oefening1
+            // Plaats hier je oplossing voor oefening 1
+
+            
+            #endregion
+
+            MainOefening2(); //blijf hier af. Plaats je oplossing van oef 2 hieronder tussen de region
+
+           
+        }
+
+
+        #region Oefening2
+        private static void MainOefening2()
+        {
+            // Plaats hier je oplossing voor oefening 2
+        }
+
+        #endregion
+
+
+    }
+}
